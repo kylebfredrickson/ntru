a0bbea9603de39afb89e490fdec924724fef9f33
Kyle Fredrickson <kyfredri@ucsc.edu>
2021-02-23 12:04:31 -0800

    Completed polynomial class with inverses, examples

--- polynomial.py
+++ polynomial.py
@@ -20,31 +20,27 @@ class Polynomial:
         else:
             self.coefficients = np.array(coefficients)
 
-    def degree(self):
-        nonzero = np.flatnonzero(self.coefficients)
-        if (len(nonzero) != 0):
-            return int(nonzero[len(nonzero) - 1])
-        else:
-            return 0
+    ### Boolean ###
 
     def __eq__(self, other):
-        self._trim()
-        other._trim()
-        return np.array_equal(self.coefficients, other.coefficients)
+        if type(other) is Polynomial:
+            self._trim()
+            other._trim()
+            return np.array_equal(self.coefficients, other.coefficients)
+        else:
+            raise TypeError("Bad type in __eq__")
+
+    ### Addition ###
 
     def __add__(self, other, mod = None):
         if type(other) is Polynomial:
             return self._polynomial_add(other, mod)
-        elif type(other) is int or type(other) is float:
-            return self._polynomial_add(Polynomial([other]), mod)
         else:
             raise TypeError("Bad type in __add__")
 
     def __sub__(self, other, mod = None):
         if type(other) is Polynomial:
             return self.__add__(Polynomial(-1 * other.coefficients), mod)
-        elif type(other) is int or type(other) is float:
-            return self.__add__(-1 * other, mod)
         else:
             raise TypeError("Bad type in __sub__")
 
@@ -64,16 +60,11 @@ class Polynomial:
         p._trim()
         return p
 
+    ### Multiplication ###
+
     def __mul__(self, other, mod = None):
         if type(other) is Polynomial:
             return self._polynomial_mul(other, mod)
-        elif type(other) is int or type(other) is float:
-            coeffs = other * self.coefficients
-            if mod != None:
-                coeffs %= mod
-            p = Polynomial(coeffs)
-            p._trim()
-            return p
         else:
             raise TypeError("Bad type in __mul__")
 
@@ -88,20 +79,17 @@ class Polynomial:
         acc._trim()
         return acc
 
-    def __truediv__(self, other, mod = None):
+    ### Division ###
+
+    def __floordiv__(self, other, mod = None):
         if type(other) is Polynomial:
             return self._divide_with_remainder(other, mod)[0]
-        elif type(other) is int or type(other) is float:
-            return self._divide_with_remainder(Polynomial(other), mod)[0]
         else:
-            print(type(other))
             raise TypeError("Bad type in __truediv__")
 
     def __mod__(self, other, mod = None):
-        if other is Polynomial:
+        if type(other) is Polynomial:
             return self._divide_with_remainder(other, mod)[1]
-        elif type(other) is int or type(other) is float:
-            return self._divide_with_remainder(Polynomial(other), mod)[1]
         else:
             raise TypeError("Bad type in __mod__")
 
@@ -117,6 +105,32 @@ class Polynomial:
             remainder = remainder.__sub__(p.__mul__(other, mod), mod)
         return (quotient, remainder)
 
+    ### Inverse ###
+
+    # See https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+    def inverse(self, other, mod = None):
+        t, new_t = Polynomial(0), Polynomial(1)
+        r, new_r = Polynomial(other.coefficients), Polynomial(self.coefficients)
+
+        while new_r != Polynomial(0):
+            quotient = r.__floordiv__(new_r, mod)
+            (t, new_t) = new_t, t.__sub__(quotient.__mul__(new_t, mod), mod)
+            (r, new_r) = new_r, r.__sub__(quotient.__mul__(new_r, mod), mod)
+
+        if r.degree() > 0:
+            raise ValueError("Inverse does not exist")
+
+        return t.__mul__(Polynomial(1).__floordiv__(r, mod), mod)
+
+    ### Misc. ###
+
+    def degree(self):
+        nonzero = np.flatnonzero(self.coefficients)
+        if (len(nonzero) != 0):
+            return int(nonzero[len(nonzero) - 1])
+        else:
+            return 0
+
     def _trim(self):
         nonzero = np.flatnonzero(self.coefficients)
         if (len(nonzero) != 0):
@@ -131,8 +145,27 @@ class Polynomial:
         return " + ".join(coeff_vars)
 
 def main():
-    a = Polynomial([1,0, 2])
-    b = Polynomial([1,2, 1])
+    a = Polynomial([1, 1])
+    print("Basic Polynomial Ops")
+    print(str(Polynomial(0)) + " + " + str(Polynomial(1)) + " = " + str(Polynomial(0) + Polynomial(1)))
+    print(str(a) + " + " + str(a) + " = " + str(a + a))
+    print(str(a) + " - " + str(a) + " = " + str(a - a))
+    print(str(a) + " * " + str(a) + " = " + str(a * a))
+    print(str(a) + " // " + str(a) + " = " + str(a // a))
+    print(str(a) + " % " + str(a) + " = " + str(a % a))
+
+    print()
+
+    print("Mod Polynomial Ops (same signature for all ops)")
+    b = Polynomial([3, 5])
+    print(str(b) + " + " + str(b) + " mod 3 = " + str(b.__add__(b, 3)))
+
+    print()
+
+    a = Polynomial([-1, 0, 1, 1, -1, 0, 1])
+    b = Polynomial([-1, 0, 0, 0, 0, 0, 0, 1])
+    print("Inverses w.r.t Polynomials")
+    print("Inv: " + str(a) + " w.r.t. " + str(b) + " mod 3 = " + str(a.inverse(b, 3)))
 
 if (__name__ == "__main__"):
     main()
